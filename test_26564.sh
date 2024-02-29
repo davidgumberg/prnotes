dcfabc568e12d8214e6a0073658434ace98ae782
David Gumberg <davidzgumberg@gmail.com>
2024-02-28 21:57:30 -0500

    Update 26564 notes and script

--- test_26564.sh
+++ test_26564.sh
@@ -1,13 +1,21 @@
-test_dir="$HOME/dev/test_data"
 bitcoin_dir="$HOME/dev/bitcoin"
+
+test_absolute_dir="$HOME/dev/test_data"
+test_relative_dir="test_data"
 
 for file in "$bitcoin_dir"/src/test/*_tests.cpp; do
     test_name=$(basename $file | sed 's/\.[^.]*$//')
 
-    $bitcoin_dir/src/test/test_bitcoin -t $test_name -- -testdatadir="$test_dir"
-
+    $bitcoin_dir/src/test/test_bitcoin -t $test_name -- -testdatadir="$test_absolute_dir"
     if [ $? -ne 0 ]; then
-        echo $test_name failed!
+        echo $test_name failed with absolute dir $test_absolute_dir!
         exit 1
     fi
+
+    $bitcoin_dir/src/test/test_bitcoin -t $test_name -- -testdatadir="$test_relative_dir"
+    if [ $? -ne 0 ]; then
+        echo $test_name failed with relative dir $test_relative_dir!
+        exit 1
+    fi
+
 done
